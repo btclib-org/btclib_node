0906261ff49319eb23b4fb8de7ed14ec709bad46
Giacomo Caironi <giacomo.caironi@gmail.com>
2021-03-12 20:00:55 +0100

    Split blockchain data in different files

--- btclib_node/block_db/__init__.py
+++ btclib_node/block_db/__init__.py
@@ -52,8 +52,8 @@ class BlockLocation:
     def deserialize(cls, data):
         stream = bytesio_from_binarydata(data)
         filename = stream.read(10).decode()
-        index = varint.deserialize(stream)
-        size = varint.deserialize(stream)
+        index = varint.decode(stream)
+        size = varint.decode(stream)
         return cls(filename, index, size)
 
     def serialize(self):
@@ -72,7 +72,7 @@ class FileMetadata:
     def deserialize(cls, data):
         stream = bytesio_from_binarydata(data)
         filename = stream.read(10).decode()
-        size = varint.deserialize(stream)
+        size = varint.decode(stream)
         return cls(filename, size)
 
     def serialize(self):
@@ -90,90 +90,116 @@ class BlockDB:
         self.files = {}
         self.blocks = {}
         self.rev_patches = {}
+
+        self.open_block_file = None
+        self.open_rev_file = None
+        self.file_index = 0
+
         self.init_from_db()
-        if not self.files:
-            self.files["data.blk"] = FileMetadata("data.blk", 0)
-            self.files["data.rev"] = FileMetadata("data.rev", 0)
-        self.current_block_file = None
-        self.current_rev_file = None
 
     def init_from_db(self):
         for key, value in self.db:
             if key[:1] == b"f":
-                self.files[key[1:].hex()] = FileMetadata.deserialize(value)
+                self.files[key[1:].decode()] = FileMetadata.deserialize(value)
             elif key[:1] == b"b":
                 self.blocks[key[1:].hex()] = BlockLocation.deserialize(value)
             elif key[:1] == b"r":
                 self.rev_patches[key[1:].hex()] = BlockLocation.deserialize(value)
+            elif key == b"i":
+                self.file_index = int.from_bytes(value, "big")
 
     def close(self):
         self.db.close()
 
-    def __get_file(self, filename):
-        if filename[-3:] == "blk":
-            if not self.current_block_file:
-                self.current_block_file = open(str(self.data_dir / filename), "a+b")
-            if self.current_block_file.name[-len(filename) :] != filename:
-                self.current_block_file.close()
-                self.current_block_file = open(str(self.data_dir / filename), "a+b")
-            return self.current_block_file
-        elif filename[-3:] == "rev":
-            if not self.current_rev_file:
-                self.current_rev_file = open(str(self.data_dir / filename), "a+b")
-            if self.current_rev_file.name[-len(filename) :] != filename:
-                self.current_rev_file.close()
-                self.current_rev_file = open(str(self.data_dir / filename), "a+b")
-            return self.current_rev_file
+    def __find_block_file(self):
+        new_file = False
+        if self.file_index == 0:
+            new_file = True
+        else:
+            filename = f"{self.file_index:06d}.blk"
+            file_metadata = self.files[filename]
+            if file_metadata.size > 128 * 1000 ** 2:  # 128MB
+                new_file = True
+        if new_file:
+            self.file_index += 1
+            filename = f"{self.file_index:06d}.blk"
+            file_metadata = FileMetadata(filename, 0)
+            self.files[filename] = file_metadata
+            self.db.put(b"f" + filename.encode(), file_metadata.serialize())
+            self.db.put(b"i", (self.file_index).to_bytes(2, "big"))
+        return self.__get_block_file(filename)
 
-    def __add_to_file(self, filename, data):
-        file = self.__get_file(filename)
+    def __get_block_file(self, filename):
+        if not self.open_block_file:
+            self.open_block_file = (self.data_dir / filename).open("a+b")
+        if self.open_block_file.name[-len(filename) :] != filename:
+            self.open_block_file.close()
+            self.open_block_file = (self.data_dir / filename).open("a+b")
+        return self.open_block_file
+
+    def __find_rev_file(self):
+        filename = f"{self.file_index:06d}.rev"
+        if filename not in self.files:
+            file_metadata = FileMetadata(filename, 0)
+            self.files[filename] = file_metadata
+            self.db.put(b"f" + filename.encode(), file_metadata.serialize())
+        return self.__get_rev_file(filename=f"{self.file_index:06d}.rev")
+
+    def __get_rev_file(self, filename):
+        if not self.open_rev_file:
+            self.open_rev_file = (self.data_dir / filename).open("a+b")
+        if self.open_rev_file.name[-len(filename) :] != filename:
+            self.open_rev_file.close()
+            self.open_rev_file = (self.data_dir / filename).open("a+b")
+        return self.open_rev_file
+
+    def __add_data_to_file(self, file, data):
         file.write(data)
         file.flush()
-        file_metadata = self.files[filename]
+        file_metadata = self.files[file.name[-10:]]
         data_index = file_metadata.size
         data_size = len(data)
         file_metadata.size += data_size
         self.db.put(b"f" + file_metadata.filename.encode(), file_metadata.serialize())
         return data_index, data_size
 
-    def __get_from_file(self, filename, index, size):
-        file = self.__get_file(filename)
+    def __get_data_from_file(self, file, index, size):
         file.seek(index)
         data = file.read(size)
         return data
 
     def add_block(self, block):
         data = block.serialize()
-        filename = "data.blk"
-        index, block_size = self.__add_to_file(filename, data)
-        block_location = BlockLocation(filename, index, block_size)
+        file = self.__find_block_file()
+        index, block_size = self.__add_data_to_file(file, data)
+        block_location = BlockLocation(file.name[-10:], index, block_size)
         self.blocks[block.header.hash] = block_location
-        self.db.put(b"f" + bytes.fromhex(block.header.hash), block_location.serialize())
+        self.db.put(b"b" + bytes.fromhex(block.header.hash), block_location.serialize())
 
     def add_rev_block(self, rev_block):
         data = rev_block.serialize()
-        filename = "data.rev"
-        index, block_size = self.__add_to_file(filename, data)
-        block_location = BlockLocation(filename, index, block_size)
+        file = self.__find_rev_file()
+        index, block_size = self.__add_data_to_file(file, data)
+        block_location = BlockLocation(file.name[-10:], index, block_size)
         self.rev_patches[rev_block.hash] = block_location
-        self.db.put(b"f" + bytes.fromhex(rev_block.hash), block_location.serialize())
+        self.db.put(b"r" + bytes.fromhex(rev_block.hash), block_location.serialize())
 
     def get_block(self, hash):
         if hash not in self.blocks:
             return None
-        filename = "data.blk"
         block_location = self.blocks[hash]
-        block_data = self.__get_from_file(
-            filename, block_location.index, block_location.size
+        file = self.__get_block_file(block_location.filename)
+        block_data = self.__get_data_from_file(
+            file, block_location.index, block_location.size
         )
         return Block.deserialize(block_data)
 
     def get_rev_block(self, hash):
         if hash not in self.rev_patches:
             return None
-        filename = "data.rev"
         rev_patch_location = self.rev_patches[hash]
-        rev_patch_data = self.__get_from_file(
-            filename, rev_patch_location.index, rev_patch_location.size
+        file = self.__get_rev_file(rev_patch_location.filename)
+        rev_patch_data = self.__get_data_from_file(
+            file, rev_patch_location.index, rev_patch_location.size
         )
         return RevBlock.deserialize(rev_patch_data)
